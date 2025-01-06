eda6ac88b4056e3709b733cd155d2b4b88896e86
w3villa-sachin-awasthi <sachin.awasthi@w3villa.com>
2025-01-06 16:18:11 +0530

    header and nav2 done

--- src/Components/Navbar2.jsx
+++ src/Components/Navbar2.jsx
@@ -4,25 +4,35 @@ import { MdOutlinePhoneInTalk } from "react-icons/md";
 import { BsFillChatSquareTextFill } from "react-icons/bs";
 
 function Navbar2() {
-   
-  return (
-    <div className='flex items-center  h-[50px] justify-between px-4 my-4 w-[100%] bg-blue-600 text-white text-[18px]'>
-        <div className='flex  justify-between item center w-[33%] h-[100%]'>
-             <div className='flex items-center hover:bg-yellow-500 h-[100%] px-3' > <div><RiBarChartHorizontalFill/></div> All departments</div>
-             <div className='flex items-center hover:bg-yellow-500 h-[100%] px-3' >Multilevel</div>
-             <div className='flex items-center hover:bg-yellow-500 h-[100%] px-3'>Mega Menu</div>
-             <div className='flex items-center hover:bg-yellow-500 h-[100%] px-3'>Fullwidth</div>
+
+    return (
+        <div className='hidden md:flex items-center  h-[50px] justify-between px-4 my-4 w-[100%] bg-blue-600 text-white text-[18px]'>
+            <div className='flex  justify-between item center  h-[100%] '>
+                <div className='flex items-center hover:bg-yellow-500 h-[100%] px-3  indicator' >
+                    <span className="indicator-item badge bg-orange-600 border-0 text-white rounded-none">Sale</span>
+                    <div><RiBarChartHorizontalFill /></div> All departments
+
+                </div>
+                {/* <div className="indicator ">
+                       
+                        <button className="">inbox</button>
+                    </div> */}
+                <div className='flex items-center hover:bg-yellow-500 h-[100%] px-3' >Multilevel</div>
+                <div className='flex items-center hover:bg-yellow-500 h-[100%] px-3 indicator'>Mega Menu
+                <span className="indicator-item badge badge-secondary rounded-none">New</span>
+                </div>
+                <div className='flex items-center hover:bg-yellow-500 h-[100%] px-3'>Fullwidth</div>
+            </div>
+            <div className='flex item-center  justify-between h-[100%] '>
+                <div className='flex items-center h-[100%] hover:bg-yellow-500 px-3'>
+                    <div className='text-[1.4rem]'><MdOutlinePhoneInTalk /></div>  1.800.555.6789
+                </div>
+                <div className='flex items-center h-[100%] hover:bg-yellow-500 px-3'>
+                    <div className='mr-2 mt-1'><BsFillChatSquareTextFill /></div>  Blog
+                </div>
+            </div>
         </div>
-        <div className='flex item-center w-[15%] justify-between h-[100%] '>
-           <div className='flex items-center h-[100%] hover:bg-yellow-500 px-3'>
-          <div><MdOutlinePhoneInTalk/></div>  1.800.555.6789
-           </div>
-           <div className='flex items-center h-[100%] hover:bg-yellow-500 px-3'>
-          <div><BsFillChatSquareTextFill/></div>  Blog
-           </div>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar2
