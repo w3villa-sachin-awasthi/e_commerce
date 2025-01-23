bd2b2c7b8574cc611c5420517eb486f235028faf
w3villa-sachin-awasthi <sachin.awasthi@w3villa.com>
2025-01-23 15:08:43 +0530

    added to temporary file

---
+++ src/component3/Temp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Temp() {
+  return (
+    <div>Temp3
+        <h2>this is temporay commit
+            
+        </h2>
+    </div>
+  )
+}
+
+export default Temp
