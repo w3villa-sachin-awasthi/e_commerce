ca4f8605a08d1409fa792bafdec4bfc37a0db168
w3villa-sachin-awasthi <sachin.awasthi@w3villa.com>
2025-01-23 15:17:11 +0530

    t

--- src/component3/Temp.jsx
+++ src/component3/Temp.jsx
@@ -2,15 +2,11 @@ import React from 'react'
 
 function Temp() {
   return (
-<<<<<<< HEAD
     <div>Temp3
         <h2>this is temporay commit
             
         </h2>
     </div>
-=======
-    <div>Temp3</div>
->>>>>>> main
   )
 }
 
